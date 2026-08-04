078bdc39fe5830fc7cb8c683fd53b835c52f482c
matias <matiasbiasioli@gmail.com>
2026-08-04 00:39:17 -0300

    testing

--- backend/db.js
+++ backend/db.js
@@ -1,26 +1,26 @@
-// const mysql = require('mysql2/promise');
-
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT
-// });
-
-// module.exports = pool;
-
 const mysql = require('mysql2/promise');
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
-});
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 module.exports = pool;
+
+// const mysql = require('mysql2/promise');
+// require('dotenv').config({
+//   path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+// });
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT
+// });
+
+// module.exports = pool;
