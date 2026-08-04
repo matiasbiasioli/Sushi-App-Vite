e14620dc833591743a789e3b2b0aea398fb47064
matias <matiasbiasioli@gmail.com>
2026-08-04 00:26:49 -0300

    testing

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: process.env.production.DB_HOST,
+    user: process.env.production.DB_USER,
+    password: process.env.production.DB_PASSWORD,
+    database: process.env.production.DB_NAME,
+    port: process.env.production.DB_PORT
 });
 
 module.exports = pool;
