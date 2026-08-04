a201ac4d8578eb6a7bdfd9b3feba1a07364bc3e0
matias <matiasbiasioli@gmail.com>
2026-08-04 00:37:47 -0300

    testing

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,26 @@
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
+
+// module.exports = pool;
+
 const mysql = require('mysql2/promise');
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+});
 
 const pool = mysql.createPool({
-    host: process.env.production.DB_HOST,
-    user: process.env.production.DB_USER,
-    password: process.env.production.DB_PASSWORD,
-    database: process.env.production.DB_NAME,
-    port: process.env.production.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 module.exports = pool;
